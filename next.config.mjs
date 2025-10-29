7b95fc759e865075f1247a12d1d794f2063d1d2f
Sahid-m <sahidmunjavar.s@gmail.com>
2025-10-29 06:27:57 +0000

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  outputFileTracingIncludes: {
+    "/src/app/api//*": ["./node_modules/.prisma/client//"],
+    "/": ["./node_modules/.prisma/client/**/*"],
+    "/src/generated": ["./src/generated/**/*"],
+  },
+};
 
 export default nextConfig;
